95dbe22f7e4d4fc0cdded3429e500dd8c3fa8ae8
Masoob <masoob0085@gmail.com>
2021-12-17 13:06:11 +0530

    faculty page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { FacultyTeam } from "./components/FacultyTeam";
 import { HomeScreen } from "./components/HomeScreen/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" component={Header} exact />
       <Route path="/" component={HomeScreen} exact />
       <Route path="/" component={Footer} exact />
+      <Route path="/faculty" component={FacultyTeam} exact />
    
     </Router>
   );
